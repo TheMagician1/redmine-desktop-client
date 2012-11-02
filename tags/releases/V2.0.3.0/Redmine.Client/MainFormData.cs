62763f9d0c194c5beef1e3a3cd4aaa8bd1a06767
bas.themagician@gmail.com <bas.themagician@gmail.com@86561f23-730a-238f-b720-e77452a7406c>
2012-11-02 08:47:28 +0000

    Create Release V2.0.3.0

---
+++ tags/releases/V2.0.3.0/Redmine.Client/MainFormData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using Redmine.Net.Api.Types;
+
+namespace Redmine.Client
+{
+    public enum ApiVersion
+    {
+        V10x,
+        V11x,
+        V13x,
+        V14x,
+        V21x,
+    }
+
+    internal class MainFormData
+    {
+        public IList<Project> Projects { get; set; }
+        public IList<Issue> Issues { get; set; }
+        public IList<ProjectMembership> Members { get; set; }
+
+        public MainFormData(int projectId, bool onlyMe)
+        {
+            NameValueCollection parameters = new NameValueCollection { { "project_id", projectId.ToString() } };
+            if (RedmineClientForm.RedmineVersion >= ApiVersion.V14x)
+                Members = RedmineClientForm.redmine.GetTotalObjectList<ProjectMembership>(parameters);
+
+            if (onlyMe)
+                parameters.Add("assigned_to_id", "me");
+            Issues = RedmineClientForm.redmine.GetTotalObjectList<Issue>(parameters);
+        }
+
+        public static Dictionary<int, T> ToDictionaryId<T>(IList<T> list) where T : Identifiable<T>
+        {
+            Dictionary<int, T> dict = new Dictionary<int,T>();
+            foreach (T element in list)
+            {
+                dict.Add(element.Id, element);
+            }
+            return dict;
+        }
+        public static Dictionary<int, Y> ToDictionaryName<Y>(IList<Y> list) where Y : IdentifiableName
+        {
+            Dictionary<int, Y> dict = new Dictionary<int, Y>();
+            foreach (Y element in list)
+            {
+                dict.Add(element.Id, element);
+            }
+            return dict;
+        }
+    }
+}
