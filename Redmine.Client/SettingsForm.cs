3d37d45af405870b2f2d5a16db4186956aa38097
bas.themagician@gmail.com <bas.themagician@gmail.com@86561f23-730a-238f-b720-e77452a7406c>
2012-11-19 12:29:40 +0000

    Added Portuguese (Brazil) Language (Thanks rafaelff1)

--- Redmine.Client/SettingsForm.cs
+++ Redmine.Client/SettingsForm.cs
@@ -14,7 +14,8 @@ namespace Redmine.Client
             new System.Globalization.CultureInfo("nl"),
             new System.Globalization.CultureInfo("en"),
             new System.Globalization.CultureInfo("de"),
-            new System.Globalization.CultureInfo("cs-CZ")
+            new System.Globalization.CultureInfo("cs-CZ"),
+            new System.Globalization.CultureInfo("pt-BR")
         };
         /* api version lower then 1.1 does not support time-entry, so is not supported. */
         private List<IdentifiableName> apiVersions = new List<IdentifiableName> {
