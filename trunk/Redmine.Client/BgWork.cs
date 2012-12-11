0975821e8f39ac3c6320ced05a025ee4e1602e24
bas.themagician@gmail.com <bas.themagician@gmail.com@86561f23-730a-238f-b720-e77452a7406c>
2012-12-11 09:15:13 +0000

    Added some comment

--- trunk/Redmine.Client/BgWork.cs
+++ trunk/Redmine.Client/BgWork.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 namespace Redmine.Client
 {
+    /// <summary>
+    /// Class for storing asynchronous functions
+    /// </summary>
     class BgWork
     {
+        /// <summary>
+        /// Constructor of Background work
+        /// </summary>
+        /// <param name="name">a descriptive name for the job</param>
+        /// <param name="work">the actual function to call</param>
         public BgWork(String name, RunAsync work)
         {
             m_name = name;
