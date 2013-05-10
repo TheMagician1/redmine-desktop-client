2a15492158369d17823c4677570ca50c295ef980
bas.themagician@gmail.com <bas.themagician@gmail.com@86561f23-730a-238f-b720-e77452a7406c>
2013-05-10 07:40:03 +0000

    Create Release V2.0.5.0

---
+++ tags/releases/V2.0.5.0/Redmine.Client/IssueFormData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Redmine.Net.Api.Types;
+
+namespace Redmine.Client
+{
+
+    internal class IssueFormData
+    {
+        public IList<ProjectTracker> Trackers { get; set; }
+        public List<IssueCategory> Categories { get; set; }
+        public IList<IssueStatus> Statuses { get; set; }
+//        public List<IdentifiableName> Priorities { get; set; }
+        public List<Redmine.Net.Api.Types.Version> Versions { get; set; }
+//        public List<Assignee> Watchers { get; set; }
+        public List<ProjectMember> ProjectMembers { get; set; }
+        public IList<IssuePriority> IssuePriorities { get; set; }
+        public IList<TimeEntry> TimeEntries { get; set; }
+    }
+}
